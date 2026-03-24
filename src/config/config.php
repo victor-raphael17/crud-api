f2e17219c69b4b9451a29f4af3b87600ec7ad9b5
victor-raphael17 <limaracingteam@gmail.com>
2026-03-24 16:59:16 -0300

    change CORS IPs

--- src/config/config.php
+++ src/config/config.php
@@ -2,4 +2,4 @@
 
 $dataFile = __DIR__ . '/../../data/data.json';
 
-$allowedOrigins = ['http://localhost:5500', 'http://127.0.0.1:5500', 'http://0.0.0.0:8080', 'http://localhost:8080'];
+$allowedOrigins = ['http://0.0.0.0:8080', 'http://localhost:8080', 'http://127.0.0.1:8080'];
